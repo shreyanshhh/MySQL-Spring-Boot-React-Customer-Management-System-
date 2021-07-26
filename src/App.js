4f44cdcb7fa3fad6431c26d6235763ffcab08362
shreyanshhh <60211668+shreyanshhh@users.noreply.github.com>
2021-07-26 20:28:51 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router } from "react-router-dom";
-import Sidebar from "./Components/Sidebar";
+import {BrowserRouter as Router } from "react-router-dom";
+import InstructorApp from "./Components/InstructorApp";
 
 class App extends Component {
   render() {
     return (
       <Router>
-      	<Sidebar/>
+      	<InstructorApp />
       </Router>
+      
     );
   }
 }
 
 export default App;
+
+
 
